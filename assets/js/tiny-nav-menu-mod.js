734b7ab24029b3f2664ba7c05ad559417509010e
tamerch <samuel.boutron@gmail.com>
2015-02-23 22:22:07 +0100

    check tinynav existence
    tiny nav is created dynamically, if doesn't exist it creates a
    javascript error ... then some scripts may not execute ... for some
    reason

--- assets/js/tiny-nav-menu-mod.js
+++ assets/js/tiny-nav-menu-mod.js
@@ -3,7 +3,7 @@ jQuery.noConflict();
 	$( document ).ready(function() {
 		if ( $('.woocommerce-breadcrumb').children().size() > 0 ){ //assume we are in shop -> select the correct menu
 			$($('.menu-header-container ul li')[1]).addClass('current-menu-item current_page_item');
-			$($('.tinynav').children()[1]).selected(true)
+			if ( $('.tinynav').length != 0 ) { $($('.tinynav').children()[1]).selected(true) }
 		}
 	});
 })(jQuery);
